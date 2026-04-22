cbf1eefdef698e7ebaa331c71ef50ed69d999a3e
Miguel Arenas <miguelmmmab@gmail.com>
2026-04-22 18:34:15 +0200

    Delete source/PhiPack directory

--- source/PhiPack/src/fasta.h
+++
@@ -1,8 +0,0 @@
-#ifndef FASTA
-#define FASTA
-
-void write_fasta(FILE* cur_stream,  char **taxa_names,align_type **alignment, int num_taxa,int num_sites);
-
-void read_fasta(const char* file_name,  char ***taxa_names, align_type ***alignment, int *num_taxa, int *num_sites);
-
-#endif
